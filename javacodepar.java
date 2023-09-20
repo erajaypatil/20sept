8eae0beccbb0543845ddd0ea1854f23d9e22f357
erajaypatil <125969198+erajaypatil@users.noreply.github.com>
2023-09-20 15:19:18 +0530

    Update javacodepar.java
    here i wote a code first time

--- javacodepar.java
+++ javacodepar.java
@@ -3,7 +3,22 @@ package javabasicpracitse;
 public class javacodepar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// array code find even and odd numbers init
+
+		int a[]={1,50,40,22,13,60,80,7894,52452};
+
+		System.out.println("Even no are..........");
+
+		for(int i=0;i<a.length;i++)
+			{
+				if(a[i]%2==0)
+				{
+					system.out.println(a[i]);
+				}
+			}
+
+
+		//first time i wrote this code into the reomte git hub repositrory
 
 	}
 
