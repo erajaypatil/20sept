d3ae9b235c7b9678866ad42a540c94b1b0b94345
er.rokade@gmail.com <er.rokade@gmail.com>
2023-09-20 15:30:40 +0530

    third time chages in code

--- javacodepar.java
+++ javacodepar.java
@@ -5,7 +5,7 @@ public class javacodepar {
 	public static void main(String[] args) {
 		// array code find even and odd numbers init
 
-		int a[]={1,50,40,22,13,60,80,7894,52452};
+		int a[]={1,50,40,22,13,60,80,7894,52452,54879454,14254,658741};
 
 		System.out.println("Even no are..........");
 
@@ -13,12 +13,22 @@ public class javacodepar {
 			{
 				if(a[i]%2==0)
 				{
-					system.out.println(a[i]);
+					System.out.println(a[i]);
 				}
 			}
 
 
 		//first time i wrote this code into the reomte git hub repositrory
+		
+		System.out.println("Odd number are..........");
+		
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]%2!=0)
+			{
+				System.out.println(a[i]);
+			}
+		}
 
 	}
 
